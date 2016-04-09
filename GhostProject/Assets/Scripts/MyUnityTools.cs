7995865c900b76a76d41f2c3237fc315a15e3891
sowrd299 <ryan.ward46@gmail.com>
2016-04-09 09:30:19 -0700

    Its a HUD!
    We have a HUD!

---
+++ GhostProject/Assets/Scripts/MyUnityTools.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+
+static class MyUnityTools {
+
+    /*
+    some useful tools for unity I wrote
+    feel free to use them
+    */
+
+    public static void dflt<T>(ref T obj, string name) {
+        /*
+        sets the default value for var obj as the component of class T of game object name
+        */
+        if (obj != null) return;
+        GameObject go = GameObject.Find(name);
+        if (go != null) obj = go.GetComponent<T>();
+    }
+
+}
+
