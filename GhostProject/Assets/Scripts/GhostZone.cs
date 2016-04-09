f59642e7a4ad894b07edff15f02b84efa30b6f6c
Laicc1 <carolynclai@gmail.com>
2016-04-09 10:45:32 -0700

    Tobii Eye Stuff

---
+++ GhostProject/Assets/Scripts/GhostZone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(GazeAwareComponent))]
+public class GhostZone : MonoBehaviour
+{
+    private GazeAwareComponent _gazeAwareComponent;
+
+    protected void Start()
+    {
+        _gazeAwareComponent = GetComponent<GazeAwareComponent>();
+    }
+
+    protected void Update()
+    {
+        if (_gazeAwareComponent.HasGaze)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
