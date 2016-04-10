0c88b88f40ddca3f4daed779300a71d52cdc2356
Jason Hsieh <jasonh117@hotmail.com>
2016-04-09 17:22:52 -0700

    updated ghostZone.cs and cleaned up animation files

--- GhostProject/Assets/Scripts/GhostZone.cs
+++ GhostProject/Assets/Scripts/GhostZone.cs
@@ -1,23 +1,21 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(GazeAwareComponent))]
 public class GhostZone : MonoBehaviour
 {
-    private GazeAwareComponent _gazeAwareComponent;
 
     protected void Start()
     {
-        _gazeAwareComponent = GetComponent<GazeAwareComponent>();
     }
 
     protected void Update()
     {
-        if (_gazeAwareComponent.HasGaze)
+        if (GetComponent<GazeAwareComponent>().HasGaze)
         {
-            if (Input.GetKeyDown("space"))
-            {
-                Destroy(this.gameObject);
-            }
+            GetComponent<Animator>().Play("front");
+        }
+        else
+        {
+            GetComponent<Animator>().Play("back");
         }
     }
 }
